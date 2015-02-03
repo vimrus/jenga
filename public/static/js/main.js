7c5152836cb3e3abd373f1510c658b4c33b2b8bf
vimrus <vimrus.com@gmail.com>
2015-02-03 23:32:03 +0800

    + add ui module.

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,11 +1,41 @@
+require.config({
+    baseUrl: '',
+    paths: {
+        jquery: 'jquery/jquery-2.1.1',
+        project: "../../../ui/project",
+        login: "../../../ui/login",
+        home: "../../../ui/home",
+    }
+});
+
 require(['mmRouter'], function() {
-    var model = avalon.define({
-        $id: 'root',
-        stages: [
-            [{id:1,title:'task'}, {id:2,title:'task2'}],
-            [{id:1,title:'task'}, {id:2,title:'task2'}],
-        ],
+    avalon.templateCache.empty = "&nbsp;"
+    avalon.define({
+        $id: "root",
+        page: "empty"
+    })
+    
+    avalon.router.get("/", function(){
+        require(['home'], function() {
+            avalon.log("home")
+        });
+    })
+
+    avalon.router.get("/login", function(){
+        require(['login'], function() {
+            avalon.log("login")
+        });
+    })
+
+    avalon.router.get("/:project", function(){
+        require(['project'], function() {
+            avalon.log("project")
+        });
+    })
+
+    avalon.history.start({
+        basepath: "/"
     })
 
     avalon.scan()
-})
+});
