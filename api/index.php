9d58e614dfc8be33995cb3428fc30b964bb91b46
vimrus <vimrus.com@gmail.com>
2015-02-06 23:57:58 +0800

    * adjust routing.

--- api/index.php
+++ api/index.php
@@ -5,6 +5,18 @@ $jenga = new Jenga();
 
 $jenga->route(array(
     '/' => 'index/index',
+    '/organizations' => 'organization/create',
+    '/organizations/:organization_id' => 'organization/info',
+    '/projects/' => 'project/create',
+    '/projects/:project_id' => 'project/info',
+    '/entries' => 'entry/create',
+    '/entries/:entry_id' => 'entry/info',
+    '/tasks' => 'task/create',
+    '/tasks/:task_id' => 'task/info',
+    '/members' => 'member/create',
+    '/members/:member_id' => 'member/info',
+    '/actions' => 'action/create',
+    '/actions/:action_id' => 'action/info',
 ));
 
 $jenga->run();
